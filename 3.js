81b296d4d3140a8aa4ffe37aec787cccd6bbacdb
Doğukan Arslan <dogukan2arslan@hotmail.com>
2019-11-11 20:32:08 +0300

    3.js silindi

--- 3.js
+++
@@ -1,5 +0,0 @@
-var numbers = [12, 5, 20, 130, 44, 9, 240]
-
-const lowerThan15 = numbers.filter(item => item < 15);
-console.log(lowerThan15);
-document.getElementById("three").innerHTML = lowerThan15;
